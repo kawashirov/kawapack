7bd5220a9b28612a680c983fc39f2ca29d549045
kawashirov <1196609+kawashirov@users.noreply.github.com>
2021-11-25 22:00:02 +0700

    The WTF Upgrade, Minor fixes

--- Kawashirov/KawaCommons/Commons/KawaGUIUtilities.cs
+++ Kawashirov/KawaCommons/Commons/KawaGUIUtilities.cs
@@ -4,12 +4,15 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using UnityEditor;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Kawashirov {
 	public static class KawaGUIUtilities {
-
+#if UNITY_EDITOR
 
 		private static readonly MethodInfo EditorGUIUtility_GetHelpIcon;
 
@@ -137,5 +140,6 @@ namespace Kawashirov {
 			EditorGUILayout.EndHorizontal();
 			EditorGUILayout.LabelField("Discord tag: kawashirov#8363");
 		}
+#endif
 	}
 }
