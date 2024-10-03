9e1aa9a2058c54034ec5520222bf30c9715777d4
kawa <1196609+kawashirov@users.noreply.github.com>
2024-10-03 14:38:06 +0700

    BGMAudioSync: major rework, muting support, dev.architech.protv compat, validations

--- Kawashirov/KawaUdon/BGMAudioSync/BGMAudioSync.cs
+++ Kawashirov/KawaUdon/BGMAudioSync/BGMAudioSync.cs
@@ -1,100 +1,119 @@
-﻿using UnityEngine;
+using UnityEngine;
 using VRC.SDKBase;
-using VRC.Udon;
 using VRC.Udon.Common;
 using UdonSharp;
-using Kawashirov;
-using Kawashirov.Refreshables;
 using Kawashirov.Udon;
+using Kawashirov;
+using System.Linq;
 
-#if UNITY_EDITOR
-using UnityEditor;
-using UdonSharpEditor;
-#endif
 
 [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
-public class BGMAudioSync : UdonSharpBehaviour
-#if !COMPILER_UDONSHARP
-    , IRefreshable
-#endif
-{
-	public StateSwitch switch_button;
-	public AudioClip[] clips;
-	[UdonSynced] public int current_clip = 0;
-	[UdonSynced] public int current_sample = 0;
+public class BGMAudioSync : CommonUSharpBehaviour {
+	[Range(0, 1)] public float Volume = 0.05f;
+	public AudioClip[] Clips;
 
-	public AudioSource source_;
-	private string path_ = "";
+	[Space]
 
-	void Start()
-	{
-		path_ = _GetPath(transform);
+	[Tooltip("Toggle BGM manually by your code.")]
+	public bool PlayBGM = true;
 
-		source_ = GetComponent<AudioSource>();
+	[Tooltip("(Optional) Use StateSwitch state to toggle BGM.")]
+	public StateSwitch SwitchButton;
 
-		float length = 0;
-		for (var i = 0; i < clips.Length; ++i)
-		{
-			var clip = clips[i];
-			clip.LoadAudioData();
-			length += clip.length;
+	[Tooltip("(Optional) Use TVPlugins for ArchiTechPro to mute BGM while video playing.")]
+	public BGMAudioSync_ArchiTechProTVBridge[] ArchiTechProTVBridges;
+
+	[Space]
+
+	[ReadOnly][UdonSynced] public int CurrentClip = 0;
+	[ReadOnly][UdonSynced] public int CurrentSample = 0;
+	[ReadOnly] public AudioSource Source;
+
+	public override void Start() {
+		base.Start();
+		logName = "Kawa|BGMAudioSync";
+
+		if (_EnsureValid(SwitchButton, "SwitchButton is invalid!")) {
+			var event_name = nameof(_UpdateVolume);
+			var curr_name = SwitchButton.EventName;
+			if (!string.Equals(curr_name, event_name)) {
+				_Warning($"Updating SwitchButton.eventName: {curr_name} -> event_name");
+				SwitchButton.EventName = event_name;
+			}
+			SendCustomEventDelayedSeconds(nameof(_SetDefault), 0.9f);
 		}
-		Debug.LogFormat(gameObject, "Loaded {1} audio clips, total time: {2} @ {0}", path_, clips.Length, length);
 
-		switch_button.currentState = 1;
-		switch_button._UpdateState();
+		if (_EnsureAll(ArchiTechProTVBridges, nameof(ArchiTechProTVBridges)))
+			foreach (var bridge in ArchiTechProTVBridges)
+				if (Utilities.IsValid(bridge))
+					bridge.Main = this;
+
+		Source = GetComponent<AudioSource>();
+		_EnsureValid(Source, true, "AudioSource is invalid!");
+
+		if (_EnsureValid(Clips, true, "Clips array is invalid!")) {
+			float length = 0;
+			_Ensure(Clips.Length > 0, true, "Clips array is empty!");
+			for (var i = 0; i < Clips.Length; ++i) {
+				var clip = Clips[i];
+				clip.LoadAudioData();
+				length += clip.length;
+			}
+			_Info($"Loaded {Clips.Length} audio clips, total time: {length}");
+		}
 	}
 
-	private void Update()
-	{
-		if (source_.isPlaying)
+	public void _SetDefault() {
+		if (Utilities.IsValid(SwitchButton)) {
+			SwitchButton.CurrentState = 1;
+			SwitchButton._UpdateState();
+		}
+		SendCustomEventDelayedFrames(nameof(_UpdateVolume), 1);
+	}
+
+	public void Update() {
+		if (Source.isPlaying)
 			return;
 		if (!Networking.IsOwner(gameObject))
 			return;
 
-		var next_clip = Random.Range(0, clips.Length);
-		if (next_clip == current_clip)
-			next_clip = (next_clip + 1) % clips.Length;
-		current_clip = next_clip;
-		current_sample = 0;
-		var clip = clips[current_clip];
-		Debug.LogFormat(gameObject, "Choosen new audio clip #{1} (\"{2}\") @ {0}", path_, current_clip, clip.name);
+		var next_clip = Random.Range(0, Clips.Length);
+		if (next_clip == CurrentClip)
+			next_clip = (next_clip + 1) % Clips.Length;
+		CurrentClip = next_clip;
+		CurrentSample = 0;
+		var clip = Clips[CurrentClip];
+		_Info($"Choosen new audio clip #{CurrentClip} (\"{clip.name}\").");
 
-		source_.Stop();
-		source_.clip = clip;
-		source_.timeSamples = 0;
-		source_.Play();
-		source_.timeSamples = 0;
+		Source.Stop();
+		Source.clip = clip;
+		Source.timeSamples = 0;
+		Source.Play();
+		Source.timeSamples = 0;
 
 		RequestSerialization();
 	}
 
-	public override void OnPreSerialization()
-	{
-		Debug.LogFormat(gameObject, "Serializing audio data: clip={1}, sample={2} @ {0}", path_, current_clip, current_sample);
-		current_sample = source_.timeSamples;
+	public override void OnPreSerialization() {
+		_Info($"Serializing audio data: clip={CurrentClip}, sample={CurrentSample}.");
+		CurrentSample = Source.timeSamples;
 	}
 
-	public override void OnDeserialization()
-	{
-		Debug.LogFormat(gameObject, "Received audio data: clip={1}, sample={2} @ {0}", path_, current_clip, current_sample);
-		source_.Stop();
-		source_.clip = clips[current_clip];
-		source_.timeSamples = current_sample;
-		source_.Play();
-		source_.timeSamples = current_sample;
+	public override void OnDeserialization() {
+		_Info($"Received audio data: clip={CurrentClip}, sample={CurrentSample}.");
+		Source.Stop();
+		Source.clip = Clips[CurrentClip];
+		Source.timeSamples = CurrentSample;
+		Source.Play();
+		Source.timeSamples = CurrentSample;
 	}
 
-	public override void OnPostSerialization(SerializationResult result)
-	{
-		if (result.success)
-		{
-			Debug.LogFormat(gameObject, "Successfuly serialized {1} bytes of audio data. @ {0}", path_, result.byteCount);
-		}
-		else
-		{
+	public override void OnPostSerialization(SerializationResult result) {
+		if (result.success) {
+			_Info($"Successfuly serialized {result.byteCount} bytes of audio data.");
+		} else {
 			// Повторить попытку через 5 сек
-			Debug.LogFormat(gameObject, "Failed to serialize audio data. @ {0}", path_);
+			_Info($"Failed to serialize audio data.");
 			SendCustomEventDelayedSeconds("_RequestSerializationDelayed", 5);
 		}
 	}
@@ -109,52 +128,61 @@ public class BGMAudioSync : UdonSharpBehaviour
 
 	public void _RequestSerializationDelayed() => RequestSerialization();
 
-	public void _UpdateState()
-	{
-		var state = switch_button.currentState == 1;
-		source_.volume = state ? 0.05f : 0.0f;
+	public bool _ShouldMuteBGM() {
+		if (!PlayBGM)
+			return false;
+
+		if (Utilities.IsValid(SwitchButton) && SwitchButton.CurrentState != 1)
+			return false;
+
+		if (Utilities.IsValid(ArchiTechProTVBridges))
+			foreach (var bridge in ArchiTechProTVBridges)
+				if (Utilities.IsValid(bridge) && bridge._ShouldMuteBGM())
+					return false;
+
+		return true;
 	}
 
-	/* Utils */
-
-	private string _GetPath(Transform t)
-	{
-		var path = t.name;
-		while (t.parent != null)
-		{
-			t = t.parent;
-			path = t.name + "/" + path;
-		}
-		return path;
+	public void _UpdateVolume() {
+		var must_v = _ShouldMuteBGM() ? Volume : 0.0f;
+		var curr_v = Source.volume;
+		if (!Mathf.Approximately(curr_v, must_v))
+			Source.volume = must_v;
 	}
-
 
 #if !COMPILER_UDONSHARP && UNITY_EDITOR
 
-	[CustomEditor(typeof(BGMAudioSync))]
-	public class Editor : UnityEditor.Editor
-	{
-		public override void OnInspectorGUI()
-		{
-			if (UdonSharpGUI.DrawDefaultUdonSharpBehaviourHeader(target))
-				return;
-			DrawDefaultInspector();
-			this.EditorRefreshableGUI();
+	private bool Validate_Clips() => KawaUdonUtilities.DistinctArray(this, nameof(Clips), ref Clips);
+
+	private void Validate_SwitchButton() {
+		if (Utilities.IsValid(SwitchButton))
+			KawaUdonUtilities.EnsureAppendedAsUdonBehaviour(SwitchButton,
+				nameof(SwitchButton.EventReceivers), ref SwitchButton.EventReceivers, this);
+	}
+
+	private bool Validate_Bridges() {
+		if (!Utilities.IsValid(ArchiTechProTVBridges)) {
+			ArchiTechProTVBridges = new BGMAudioSync_ArchiTechProTVBridge[0];
+			return true;
+		}
+		var bridges = ArchiTechProTVBridges.UnityNotNull().RuntimeOnly().Distinct().ToArray();
+		return KawaUdonUtilities.ModifyArray(this, nameof(ArchiTechProTVBridges), ref ArchiTechProTVBridges, bridges);
+	}
+
+	private void Validate_Bridges_Entries(BGMAudioSync_ArchiTechProTVBridge bridge) {
+		if (Utilities.IsValid(bridge) && bridge.Main != this) {
+			// TODO Ensure Set + warning
+			bridge.Main = this;
+			bridge.ApplyProxyModificationsAndSetDirty();
 		}
 	}
 
-	public void Refresh()
-	{
-		KawaUdonUtilities.DistinctArray(this, nameof(clips), ref clips);
-
-		if (Utilities.IsValid(switch_button)) {
-			KawaUdonUtilities.EnsureAppendedAsUdonBehaviour(switch_button, nameof(switch_button.eventReceivers), ref switch_button.eventReceivers, this);
-		}
+	public override void Refresh() {
+		KawaUdonUtilities.ValidateSafe(Validate_Clips, this, nameof(Clips));
+		KawaUdonUtilities.ValidateSafe(Validate_SwitchButton, this, nameof(SwitchButton));
+		KawaUdonUtilities.ValidateSafe(Validate_Bridges, this, nameof(ArchiTechProTVBridges));
+		KawaUdonUtilities.ValidateSafeForEach(ArchiTechProTVBridges, Validate_Bridges_Entries, this, nameof(ArchiTechProTVBridges));
 	}
-
-	public UnityEngine.Object AsUnityObject() => this;
-
-	public string RefreshablePath() => gameObject.KawaGetFullPath();
 
 #endif
 }
---
+++ Kawashirov/KawaUdon/BGMAudioSync/BGMAudioSync_ArchiTechProTVBridge.cs
@@ -0,0 +1,97 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+using Kawashirov;
+using Kawashirov.Udon;
+
+/*
+	Не используем using ArchiTech.ProTV;
+	Потому что удон сосёт хуй и не подсасывает дефайны 
+	из юнити асембли дефенишин в удон асембли дефенишин 
+*/
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class BGMAudioSync_ArchiTechProTVBridge : CommonUSharpBehaviour {
+	public UdonBehaviour TVManager;
+	public BGMAudioSync Main;
+
+	[Space]
+	[ReadOnly] public bool IsPlaying = false;
+	[ReadOnly] public bool IsMuted = false;
+	[ReadOnly] public float Volume = 1.0f;
+
+	[Space]
+	[ReadOnly] public float OUT_VOLUME = 1.0f;
+
+	public override void Start() {
+		base.Start();
+		logName = "Kawa|BGMAudioSync|ArchiTechProTVPlugin";
+
+		IsPlaying = false;
+		IsMuted = false;
+		Volume = 1.0f;
+
+		OUT_VOLUME = 1.0f;
+
+		if (_EnsureValid(TVManager, true, "TVManager is invalid!")) {
+			TVManager.SetProgramVariable("IN_LISTENER", this);
+			TVManager.SendCustomEventDelayedSeconds("_RegisterListener", 1f);
+		}
+
+		_EnsureValid(Main, "Main is invalid!");
+	}
+
+	public void _TvPlay() {
+		IsPlaying = true;
+		_NotifyMain();
+	}
+
+	public void _TvPause() {
+		IsPlaying = false;
+		_NotifyMain();
+	}
+
+	public void _TvStop() {
+		IsPlaying = false;
+		_NotifyMain();
+	}
+
+	public void _TvMute() {
+		IsMuted = true;
+		_NotifyMain();
+	}
+
+	public void _TvUnMute() {
+		IsMuted = true;
+		_NotifyMain();
+	}
+
+	public void _TvVolumeChange() {
+		Volume = OUT_VOLUME;
+		_NotifyMain();
+	}
+
+	private void _NotifyMain() {
+		if (Utilities.IsValid(Main))
+			Main.SendCustomEventDelayedFrames(nameof(Main._UpdateVolume), 1);
+	}
+
+	public bool _ShouldMuteBGM() {
+		return IsPlaying && Volume > Mathf.Epsilon && !IsMuted;
+	}
+
+
+#if !COMPILER_UDONSHARP && UNITY_EDITOR
+
+	public override void Refresh() {
+		if (Utilities.IsValid(Main)) {
+			KawaUdonUtilities.EnsureAppended(Main,
+				nameof(Main.ArchiTechProTVBridges), ref Main.ArchiTechProTVBridges, this);
+		}
+	}
+
+#endif
+}
+
