d9e546bfc4a88dd9c0c6803cab5c94fa6142c98c
kawashirov <1196609+kawashirov@users.noreply.github.com>
2024-12-06 11:52:45 +0700

    SelectableSpawn wip

---
+++ Kawashirov/KawaUdon/SelectableSpawn/SelectableSpawn.cs
@@ -0,0 +1,72 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using Kawashirov;
+using Kawashirov.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class SelectableSpawn : CommonUSharpBehaviour {
+	//public Desc
+	public StateSwitch SwitchButton;
+	public SpawnGroup[] SpawnGroups;
+	public string AutoBindSpawnGroupProgram = "SpawnGroup";
+
+	public override void Start() {
+		base.Start();
+		logName = "SelectableSpawn";
+
+		_Ensure(SwitchButton, "SwitchButton is invalid!");
+		_EnsureCountValid(SpawnGroups, true, nameof(SpawnGroups));
+	}
+
+	public void _RespawnLocalPlayer(VRCPlayerApi player) {
+		if (!(Utilities.IsValid(player) && player.isLocal))
+			return;
+
+		if (!Utilities.IsValid(SpawnGroups)) {
+			_Error($"Can't respawn player: {nameof(SpawnGroups)} is invalid!");
+			return;
+		}
+
+		var group_idx = SwitchButton.CurrentState % SpawnGroups.Length;
+		var group = SpawnGroups[group_idx];
+		if (!Utilities.IsValid(group)) {
+			_Error($"Can't respawn player: {nameof(SpawnGroups)}[{group_idx}] is invalid!");
+			return;
+		}
+
+		var spawn = group._PickRandom();
+		if (!Utilities.IsValid(spawn)) {
+			_Error($"Can't respawn player: {nameof(SpawnGroups)}[{group_idx}] picked invalid spawn!");
+			return;
+		}
+
+		_Info($"Respawning at {nameof(SpawnGroups)}[{group_idx}]: {spawn}...");
+		player.TeleportTo(spawn.position, spawn.rotation);
+	}
+
+	public override void OnPlayerJoined(VRCPlayerApi player) {
+		_RespawnLocalPlayer(player);
+	}
+
+	public override void OnPlayerRespawn(VRCPlayerApi player) {
+		_RespawnLocalPlayer(player);
+	}
+
+#if !COMPILER_UDONSHARP && UNITY_EDITOR
+	private bool Validate_AutoBindSpawnGroupProgram() {
+		KawaUdonUtilities.ValidProgramName(AutoBindSpawnGroupProgram, $"{nameof(AutoBindSpawnGroupProgram)}", this);
+		return false;
+	}
+
+	private bool Validate_SpawnGroups() {
+		// TODO
+		return false;
+	}
+
+	public override void Refresh() {
+		KawaUdonUtilities.ValidateSafe(Validate_AutoBindSpawnGroupProgram, this, nameof(AutoBindSpawnGroupProgram));
+		KawaUdonUtilities.ValidateSafe(Validate_SpawnGroups, this, nameof(SpawnGroups));
+	}
+#endif
+}
---
+++ Kawashirov/KawaUdon/SelectableSpawn/SpawnGroup.cs
@@ -0,0 +1,22 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class SpawnGroup : UdonSharpBehaviour {
+    public Transform[] SpawnPoints;
+
+    public void Start() {
+        // literally no other code, data class
+        if (!Utilities.IsValid(SpawnPoints))
+            SpawnPoints = new Transform[0];
+    }
+
+    public Transform _PickRandom() {
+        if (!Utilities.IsValid(SpawnPoints))
+            return null;
+        var len = SpawnPoints.Length;
+        return len > 1 ? SpawnPoints[Random.Range(0, len)] : len == 1 ? SpawnPoints[0] : null;
+    }
+
+}
