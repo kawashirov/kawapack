72f9a02f23ebf5ce53a2e4363c5b33e2d4a36e56
kawashirov <1196609+kawashirov@users.noreply.github.com>
2025-01-16 21:47:17 +0700

    BakeLightingSBA: Lightmapping.isRunning is not enough

--- Packages/kawashirov.commons/SceneBuilding/BakeLightingSBA.cs
+++ Packages/kawashirov.commons/SceneBuilding/BakeLightingSBA.cs
@@ -4,10 +4,22 @@ using UnityEditor;
 
 namespace Kawashirov.SceneBuilding {
 	public class BakeLightingSBA : BaseSBA {
+		protected bool bakeRegistered = false;
+		protected bool bakeCompleted = false;
+
+		protected void OnBakeCompleted() {
+			bakeCompleted = true;
+		}
+
+		protected void EnsureBakeCompletedReg() {
+			if (!bakeRegistered)
+				Lightmapping.bakeCompleted += OnBakeCompleted;
+		}
+
 		public override IEnumerator RunAsync(BuildingScenario scenario) {
 			Log($"Clearing old lightmaps...");
 
-			if (Lightmapping.isRunning) {
+			while (Lightmapping.isRunning) {
 				Lightmapping.Cancel();
 				yield return null;
 			}
@@ -15,14 +27,17 @@ namespace Kawashirov.SceneBuilding {
 			Lightmapping.ClearLightingDataAsset();
 			yield return null;
 
+			EnsureBakeCompletedReg();
+			bakeCompleted = false;
+			var loops = 0;
 			Log($"Baking new lightmaps...");
 			Lightmapping.BakeAsync();
 			yield return null;
-			var loops = 0;
-			while (Lightmapping.isRunning) {
+			while (Lightmapping.isRunning || !bakeCompleted) {
 				++loops;
 				yield return null;
 			}
+			bakeCompleted = false;
 
 			Log($"Baked new lightmaps: {loops} loops");
 		}
